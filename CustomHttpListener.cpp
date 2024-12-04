85182de5366b1667a957a7704c01eba1b677fcbb
AnimaVR <22724774+AnimaVR@users.noreply.github.com>
2024-12-04 10:51:41 +0000

    Create CustomHttpListener.cpp

---
+++ CustomHttpListener.cpp
@@ -0,0 +1,75 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Sockets.h"
+#include "SocketSubsystem.h"
+#include "HAL/Runnable.h"
+#include "Async/Async.h"
+#include "CustomHttpListener.generated.h"
+
+// Delegate to broadcast the received string to Blueprints
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnStringReceived, const FString&, ReceivedString);
+
+UCLASS()
+class MAGEGAME_API ACustomHttpListener : public AActor
+{
+    GENERATED_BODY()
+
+public:
+    ACustomHttpListener();
+
+protected:
+    virtual void BeginPlay() override;
+    virtual void EndPlay(const EEndPlayReason::Type EndPlayReason) override;
+
+    // Start the server
+    void StartHttpServer();
+
+public:
+    // Expose the received string to Blueprints
+    UPROPERTY(BlueprintAssignable, Category = "Events")
+    FOnStringReceived OnStringReceived;
+
+private:
+    FSocket* ServerSocket;
+
+    // Runnable class for managing the server thread
+    class FServerRunnable : public FRunnable
+    {
+    public:
+        FServerRunnable(FSocket* InServerSocket)
+            : ServerSocket(InServerSocket), bShouldRun(true) {
+        }
+
+        virtual ~FServerRunnable() = default;
+
+        virtual bool Init() override { return true; }
+
+        virtual uint32 Run() override;
+
+        virtual void Stop() override { bShouldRun = false; }
+
+        // Check if the thread should keep running
+        bool IsRunning() const { return bShouldRun; }
+
+        // Stop the thread
+        void SetShouldRun(bool bRun) { bShouldRun = bRun; }
+
+        // Delegate to broadcast the received string
+        DECLARE_EVENT_OneParam(FServerRunnable, FOnStringReceivedEvent, const FString&)
+        FOnStringReceivedEvent& OnStringReceived() { return StringReceivedEvent; }
+
+    private:
+        void ListenForConnections();
+
+        FSocket* ServerSocket;
+        bool bShouldRun;
+        FOnStringReceivedEvent StringReceivedEvent;
+    };
+
+    FServerRunnable* ServerRunnable;
+    FRunnableThread* ServerThread;
+
+    void HandleReceivedString(const FString& ReceivedString);
+};
